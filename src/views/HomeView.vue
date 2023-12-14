b1dc9fc24a244182e625fbaa66836eaa66b1f87e
eyadroid <46623135+eyadroid@users.noreply.github.com>
2023-12-14 16:29:29 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -176,7 +176,7 @@ export default {
       this.submit();
     },
     submit() {
-      axios.post("http://:3000/api/tweet", {
+      axios.post("https://thetufan.com/api/tweet", {
         appKey: this.appKey,
         appSecret: this.appSecret,
         accessToken: this.accessToken,
