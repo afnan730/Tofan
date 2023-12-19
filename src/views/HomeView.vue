3e9df6a122935da77cb3c2ae38f045d43d7da8bf
Afnan Abdo <79424831+afnan730@users.noreply.github.com>
2023-12-19 12:44:25 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -150,7 +150,7 @@ export default {
   },
   methods: {
     checkTimeWindow() {
-       console.log("reach me through whatsapp:+905374494558");
+       
       console.log(this.$store.state.arabicTest);
       //this.isLoading = true;
       // const now = new Date();
@@ -158,17 +158,17 @@ export default {
 
       // if (currentHour >= 21 || currentHour < 0) {
       //   console.log("Posting is allowed.");
-      //this.submit();
+      this.submit();
       // } else {
       //   console.log("The allowed posting hours are 9 PM to 12 AM");
       //    this.error="The allowed posting hours are from 9 PM to 12 AM"
       // }
-        if(this.$store.state.arabicTest){
-        this.error="تم تعليق النشر إلى حين موعد الإطلاق. متطلعون لانضمامكم إلى مجموعاتنا على مواقع التواصل الاجتماعي وتفاعلكم معنا"
-       }
-        else{
-          this.error="Posting is temporarily on hold until the launch date. Join our social media for updates and engage with us."
-        }
+       //  if(this.$store.state.arabicTest){
+       //  this.error="تم تعليق النشر إلى حين موعد الإطلاق. متطلعون لانضمامكم إلى مجموعاتنا على مواقع التواصل الاجتماعي وتفاعلكم معنا"
+       // }
+       //  else{
+       //    this.error="Posting is temporarily on hold until the launch date. Join our social media for updates and engage with us."
+       //  }
       
     },
     submit() {
