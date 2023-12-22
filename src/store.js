5280308adcac1699358ee3ad15c1f176c239d137
afnan730 <afnan6642@gmail.com>
2023-12-22 21:13:29 +0300

    tweets added

--- src/store.js
+++ src/store.js
@@ -1,66 +1,58 @@
 import { createStore } from "vuex";
-import img1 from "./assets/static/44.jpeg";
 
 import img3 from "./assets/static/66.jpeg";
 import video1 from "./assets/static/video1.mp4";
-import p3 from "./assets/static/p3.jpg";
+
 import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
 import v3 from "./assets/static/v3.mp4";
-import v5 from "./assets/static/v5.mp4";
-import v6 from "./assets/static/v6.mp4";
+import p1 from "./assets/static/p1.jpeg";
+import p4 from "./assets/static/p3.jpeg";
+import p5 from "./assets/static/44.jpeg";
 const store = createStore({
   state() {
     return {
       tweets: [
         {
-          text: "Hamas treated their prisoners well. They even exchanged their goodbyes W a contentement's smiles.This is the true tolerant face of Islam.",
-          type: "image",
-          src: img1,
-        },
-        {
-          text: "14 YEAR OLD YAZEN LOST 3 LIMBS FROM ISRAELI BOMBING",
-          type: "image",
-          src: img3,
-        },
-        {
-          text: "Attempt to rescue a young man from under the rubble of a house bombed by the occupation in the Beshara neighborhood in Deir al-Balah, central Gaza Strip, this evening",
+          text: "In all the laws of humanity, resistance to the occupier was an honor. The Zionist entity is trying to overturn moral standards. It wants to kill and terrorize people, and portray to the world that those who resist are terrorists.#Christmas #Trump New Year.",
           type: "video",
           src: video1,
         },
         {
-          text: "Sahar Caldarone, a released Israeli detainee, said in an interview: 'I told myself many times that I might die here because of Israeli bombs and not because of Hamas.",
-          type: "text",
+          text: "14 YEAR OLD YAZEN LOST 3 LIMBS FROM ISRAELI BOMBING #Christmas #Trump New Year",
+          type: "image",
+          src: img3,
         },
         {
-          text: "Israeli occupation forces raids last night on the central Gaza Strip caused a massive destruction in residential buildings",
+          text: "According to the UN charter #51, Israel doesn't have the right to self-defense against Palestinians as it occupies their land.#Christmas #Trump New Year",
           type: "video",
           src: v2,
         },
         {
-          text: "The Ministry of Health says Donia Abu Mohsen was supposed to be sent to Egypt tomorrow to get treatment there. However, an Israeli shelling on Nasser Hospital killed her two days ago. #Gaza",
-          type: "video",
-          src: v6,
-        },
-        {
-          text: "Scenes from shelter camps in the south of the #Gaza Strip",
-          type: "video",
-          src: v5,
-        },
-        {
-          text: "#BREAKING| Israeli forces detonate a house in the village of Aqraba in the #WestBank city of #Nablus.",
+          text: "You can't achieve peace with an apartheid antity that would do anything to steal your land, torture, murder, destruction, etc. The 2 states solution is the only clear path to peace.#Christmas #Trump New Year",
           type: "video",
           src: v1,
         },
         {
-          text: "The first moments of the Israeli occupation forces bombing of a house in Rafah last night.",
+          text: "The land of Palestine was never  desert, but rather an inhabited land with villages inhabited by people and communities of Muslims, Christians, and Jews, until Zionism came and decided to carry out ethnic cleansing and take their place.#Christmas #Trump New Year",
           type: "video",
           src: v3,
         },
+
         {
-          text: "The mass destruction in homes for the Zorob family in Rafah, south of the Gaza Strip, after the Israeli occupation heavily bombed the area last night.",
+          text: "Resistance forced the occupation to withdraw its strongest brigade, the Golani's. This limited resources & basic resistance astonished the world with its military tactics & abilities & managed to destroy the myth of a strong and very advanced army.#Christmas #Trump New Year",
           type: "image",
-          src: p3,
+          src: p5,
+        },
+        {
+          text: "Netanyahu promised freeing the hostages & resistance's elimination. 2+ months in & all he did is innocents' madsacre. Resistance forced their best brigade to withdraw. Israel is eager to have a truce, but it wants to keep its undefeatable image as well.#Christmas #Trump New Year",
+          type: "image",
+          src: p1,
+        },
+        {
+          text: "إن كان لواء غولاني هو أقوى الألوية، فسحبه من المعركة يضعنا أمام احتمالين: إما أنه قوي بالفعل؛ لكن المقاومة أثبتت أنها أقوى منه ميدانيا، وإما أنه أُعطِي مكانة لا يستحقها، وهذا يعني أن بقية الألوية والفرق هشة، وسيتم سحبها قريبا. #النصر_الاتفاق #يوم_الجمعه #محمد #غزه_تستغيث.",
+          type: "image",
+          src: p4,
         },
       ],
       arabic: {
@@ -75,7 +67,7 @@ const store = createStore({
         modalButton: "إغلاق",
         note: "عند الضغط على نشر التغريدات سيتم نشرها على حسابك في تويتر",
         LoadingMsg:
-          "يرجى عدم مغادرة الصفحة حتى يتم النشر، قد يستغرق ذلك من دقيقة إلى دقيقتين بسبب حجم الفيدوهات ",
+          "يرجى عدم مغادرة الصفحة حتى يتم النشر، قد يستغرق ذلك بعض الوقت بسبب حجم الفيدوهات ",
       },
       english: {
         header: "Truth Flood🔻",
@@ -89,7 +81,7 @@ const store = createStore({
         modalButton: "close",
         note: "After clicking Post tweets, they will be posted on your Twitter account",
         LoadingMsg:
-          "Please don't leave the page until you see the success message. This may take approximately 1 to 2 minutes due to media size",
+          "Please don't leave the page until you see the success message. This may take a bit of time due to media size",
       },
       arabicTest: true,
     };
