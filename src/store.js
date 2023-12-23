651337b1179317954143d1996708e79ca11d3f24
afnan730 <afnan6642@gmail.com>
2023-12-23 22:00:45 +0300

    new tweeet

--- src/store.js
+++ src/store.js
@@ -14,17 +14,17 @@ const store = createStore({
     return {
       tweets: [
         {
-          text: "She is quiet, yet her eyes speak volumes #Christmas #Trump New Year.",
+          text: "She is quiet, yet her eyes speak volumes #Ortega #Christmas #Santa.",
           type: "video",
           src: video1,
         },
         {
-          text: "البارحة فجّر الاحتلال محيط مسجد فلسطين في غزّة، المسجد ذاته ومحيطه الذي شهد وانطلقت منه مشهد جنازة العيّاش العظيمة، هذا حقد ممتد، والحقد وقود المعارك، ونحن أولى به منهم. إن نسيتم أذى العدوّ هلكتم فتواصوا بالحقد جيلاً فجيلا ",
+          text: "البارحة فجّر الاحتلال محيط مسجد فلسطين في غزّة، المسجد ذاته ومحيطه الذي شهد وانطلقت منه مشهد جنازة العيّاش العظيمة، هذا حقد ممتد، والحقد وقود المعارك، ونحن أولى به منهم. إن نسيتم أذى العدوّ هلكتم فتواصوا بالحقد جيلاً فجيلا #غزه_تنتصر# محمد #معتز",
           type: "image",
           src: img3,
         },
         {
-          text: "Danny Neumann, a former Knesset member, advocates 4 exterminating every man, woman & child in Gaza.#Christmas #Trump New Year",
+          text: "Danny Neumann, a former Knesset member, advocates for exterminating every man, woman and child in Gaza.This is the fact of israeli occupation, based on the principle of apartheid. #Ortega #Christmas #Santar",
           type: "video",
           src: v2,
         },
@@ -34,21 +34,21 @@ const store = createStore({
         //   src: v1,
         // },
         {
-          text: "The Resistance destroyed not only the occupation's advanced machinery but the mythe of a very strong army in the world.#Christmas #Trump New Year",
+          text: "The Resistance destroyed not only the occupation's advanced machinery but the mythe of a very strong army in the world.#Ortega #Christmas #Santa",
           type: "video",
           src: v3,
         },
 
         {
-          text: "According 2 the Palestinian Central Bureau of Statistics, approved by the UNCA's General Assembly in 2000, more than 6.4 million Palestinian R refugees since 1948 due 2 the forced displacement by Israeli apartheid regime.#Christmas #Trump New Year",
+          text: "According 2 the Palestinian Central Bureau of Statistics, approved by the UNCA's General Assembly in 2000, more than 6.4 million Palestinian R refugees since 1948 due 2 the forced displacement by Israeli apartheid regime.#Ortega #Christmas #Santa",
           type: "image",
           src: p5,
         },
-        // {
-        //   text: "Netanyahu promised freeing the hostages & resistance's elimination. 2+ months in & all he did is innocents' madsacre. Resistance forced their best brigade to withdraw. Israel is eager to have a truce, but it wants to keep its undefeatable image as well.#Christmas #Trump New Year",
-        //   type: "image",
-        //   src: p1,
-        // },
+        {
+          text: "With the US elections approaching, people express their disapproval 2 all candidates who adopt a unified pro Israrli position that justified genocide & war crimes. #Ortega #Christmas #Santa",
+          type: "image",
+          src: p1,
+        },
         // {
         //   text: "إن كان لواء غولاني هو أقوى الألوية، فسحبه من المعركة يضعنا أمام احتمالين: إما أنه قوي بالفعل؛ لكن المقاومة أثبتت أنها أقوى منه ميدانيا، وإما أنه أُعطِي مكانة لا يستحقها، وهذا يعني أن بقية الألوية والفرق هشة، وسيتم سحبها قريبا. #النصر_الاتفاق #يوم_الجمعه #محمد #غزه_تستغيث.",
         //   type: "image",
