b5ed1345b002cda73cf5dcb381f6d0d9e29685fe
Kaser2023 <75515943+Kaser2023@users.noreply.github.com>
2023-12-24 15:15:54 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -175,7 +175,7 @@ export default {
       //    this.error="The allowed posting hours are from 9 PM to 12 AM"
       // }
         if(this.$store.state.arabicTest){
-          this.error="تم توقيف النشر حتى الساعة 9 مساءً. نتطلّع لرؤيتكم"
+          this.error="تم إيقاف النشر حتى الساعة 9 مساءً. نتطلّع لرؤيتكم"
         }
         else{
            this.error="Posting tweets has been suspended until 9 p.m. We look forward to seeing you."
