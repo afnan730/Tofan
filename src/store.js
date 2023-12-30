592acf19f6fba7f440cfa063f5c383c5a6e2fb75
afnan730 <afnan6642@gmail.com>
2023-12-30 20:18:34 +0300

    new tweets

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from "vuex";
 
 import v1 from "./assets/static/v1.mp4";
-import v2 from "./assets/static/v2.mp4";
+// import v2 from "./assets/static/v2.mp4";
 import p4 from "./assets/static/p4.jpeg";
 import p1 from "./assets/static/p1.jpeg";
 import p3 from "./assets/static/p3.jpeg";
@@ -11,94 +11,89 @@ import p6 from "./assets/static/p6.jpeg";
 import p7 from "./assets/static/p7.jpeg";
 import p8 from "./assets/static/p8.jpeg";
 import p9 from "./assets/static/p9.jpeg";
-import v3 from "./assets/static/v3.mp4";
-import v4 from "./assets/static/v4.mp4";
-import v5 from "./assets/static/v5.mp4";
+// import v3 from "./assets/static/v3.mp4";
+// import v4 from "./assets/static/v4.mp4";
+// import v5 from "./assets/static/v5.mp4";
 import p10 from "./assets/static/p10.jpeg";
 const store = createStore({
   state() {
     return {
       tweets: [
         {
-          text: "Ilan pappe, Israeli historian refuted 10 Israeli myths:1-Palestine was an empty land: 'As per Yonatan Mendel about Ottoman records:a total population of 462,465 resided in Palestine in 1878. Of this no.  87% were Muslim, 10% Christians & 3% Jewish' #Slavery #Christmas #XRC20",
+          text: "The King Crane Commission rejects the Zionist claim of a 2000-year-old right for Jews in Palestine. It advocates limiting Jewish migration and ending the establishment of a purely Jewish state in the region #Term #Palmer #Chelsea #Luton #Gaza",
+          type: "image",
+          src: p5,
+        },
+        {
+          text: "Understanding Palestinians' cause isn't so complicated. They're the indigenous of a land & a group of Zionists who stole it to establish their state. But that wasn’t enough 4 those homes wreckers, as they're trying to wipe Palestinians as well! #Term #Palmer #Chelsea #Luton #Gaza",
+          type: "image",
+          src: p3,
+        },
+        {
+          text: "Palastinians in Israeli's prisons suffer from  bad and inhumane conditions. Lately, they are being treated as lab rates & forced to participate in drugs' trail that worsened their health. #Term #Palmer #Chelsea #Luton #Gaza",
           type: "image",
           src: p7,
         },
         {
-          text: "The 7th of Oct in Gaza wasn't the beginning of the Palestinians' struggle against the Zionist occupation it's an age-old war. Repression of the Palestinian Revolution by Britain helped the establishment of the occupying Power in 1948. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "image",
-          src: p4,
-        },
-        {
-          text: "On behalf of the whole world, Gaza alone is facing the worst evil humanity ever known. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+          text: "Killing, for them, is a religious ideology!What logic is there to justify genocides?Where are all the human rights organizations regarding what's happening in Gaza? Aren't they supposed to stand up with the oppressed & protect them? #Term #Palmer #Chelsea #Luton #Gaza",
           type: "video",
           src: v1,
         },
         {
-          text: "Israel's genocide on Gaza exposed the double standards and hypocrisy of human rights organizations, which are supposed to stand and protect the opressed not to abandon and neglect them. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+          text: "Ben Gurion,the founder of Israel,said:'I support the forced deportation of Palestinians,& I don't see anything immoral in it.' So,4 Israelis, Palestinians've no right 2 Palastine & they r the obstacle 2 a purely Jewish State's establishment. #Term #Palmer #Chelsea #Luton #Gaza",
           type: "image",
-          src: p5,
-        },
-
-        {
-          text: "Gaza's genocide reveals another ugly reality of the international policies: criminals that should be prescutd based on their war crimes,R let free if they'r deemed 2 be powerful or play an important rule on others players interests & benefits #Slavery #Christmas #Pamplona #XRC20",
-          type: "image",
-          src: p2,
+          src: p4,
         },
         {
-          text: "Liberating Palestine is a liberation for the world. Peace will not occur until it is for everyone. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "image",
-          src: p6,
-        },
-        {
-          text: "Palestinian Red Crescent spokesman: The Israeli occupation army does not stop targeting UNRWA schools that provide shelter for displaced persons. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "image",
-          src: p9,
-        },
-        {
-          text: "'I'm not afraid of death but to be a captive', a Gazan said. The occupation kidnapped dozens of Gazans, who were sent back in body bags with no kidneys, hearts, etc. What level of inhumanity & cruelty is this? #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "image",
-          src: p8,
-        },
-        {
-          text: "If all these scenes won't make the world do something to end those children' suffering, what would? They're already facing death either by bombardment, diseases, starvation, fear & bad weather conditions. Ceasefire now 2 save them. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "video",
-          src: v2,
-        },
-
-        {
-          text: "Since Oct 7th, 110 ambulance&145 health establishments were bombed, 320 victims from medical staff as well as 25 hospitals were totally out of service & as a result of all this damages 2 health care system, hundered of wounded & patients r dying. #Slavery #Christmas #StormGerrit",
-          type: "image",
-          src: p3,
-        },
-
-        {
-          text: "'A child is killed every 10 minutes in Gaza.'This is what the World Health Organization(WHO)stated,In light of a genocide that exceeded 1440 hours, in just over two months! #Slavery #StormGerrit #Christmas #Pamplona",
+          text: "Israel frequently engages in projection, falsely accusing the resistance of using children as human shields. In facts, evidence proves that the Israeli army employs this tactic, and use children to shield their tanks, among other instances. #Term #Palmer #Chelsea #Luton #Gaza.",
           type: "image",
           src: p1,
         },
         {
-          text: "Children should never be exposed to terrific scenes as per Children Right Organisations. However, in Gaza, they don't only witness them but live them if they got lucky and escape death. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "video",
-          src: v3,
-        },
-
-        {
-          text: "In a 5 years operation,'Bid Rig', FBI made 46 arrests of high profile people including politicians, mayors & Orthodox rabbis who were charged with money laundering 2 Israel, black market kidney trafficking & real state scheme. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+          text: "Being a Gazan, you are deemed a terrorist & a target. Occupation's army kills indiscriminately everyone (children,  women, elderly, etc) & destroyed everything in their way and then create a new fabrication to mask the truth. #Term #Palmer #Chelsea #Luton #Gaza",
           type: "image",
-          src: p10,
-        },
-        {
-          text: "Even the bodies of martyrs in Gaza did not escape the brutality of the occupying entity, as they were mutilated, their internal organs were stolen, and many of them decomposed. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
-          type: "video",
-          src: v4,
+          src: p2,
         },
 
         {
-          text: "Bombardment isn't the only death's cause in Gaza.  According 2 Euro-Mediterranean Human Rights Monitor, 71% Gazans r facing food shortage, 98% suffer from starvation while 64% consume grass, raw & or expired food. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+          text: "Washington & Europe's support 4 Israeli occupation in its barbaric attacks on Gaza, where no one is safe,is a clear indication of double standards & hypocrisy. It is the real language that major players & politicians speak & understand #Term #Palmer #Chelsea #Luton #Gaza",
+          type: "image",
+          src: p6,
+        },
+
+        {
+          text: "According to the Palestinian Red Crescent spokesman, the entire northern Gaza Strip lacks a single ambulance, despite the northern Gaza Strip is crowded with wounded and martyrs daily as a result of the ongoing criminality of the occupation. #Term #Palmer #Chelsea #Luton #Gaza",
+          type: "image",
+          src: p9,
+        },
+        {
+          text: "Bombardment isn't the only death's cause in Gaza.  According to Euro-Mediterranean Human Rights Monitor, 71% Gazans are facing food shortage, 98% suffer from starvation while 64% consume grass, raw & or expired food. #Term #Palmer #Chelsea #Luton #Gaza",
           type: "image",
           src: p8,
         },
+
+        // {
+        //   text: "Children should never be exposed to terrific scenes as per Children Right Organisations. However, in Gaza, they don't only witness them but live them if they got lucky and escape death. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+        //   type: "video",
+        //   src: v3,
+        // },
+
+        {
+          text: "Gaza isn't Israel's sole target. Masjid AlAqsa faces continuous aggression 4 decades; on 12/28/2023, 174 settlers stormed it without restraint. Muslims endure inhumane treatment, humiliation, aggressive searches, assault,& unjustified entry denials. #Term #Palmer #Chelsea #Luton",
+          type: "image",
+          src: p10,
+        },
+        // {
+        //   text: "Even the bodies of martyrs in Gaza did not escape the brutality of the occupying entity, as they were mutilated, their internal organs were stolen, and many of them decomposed. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+        //   type: "video",
+        //   src: v4,
+        // },
+
+        // {
+        //   text: "Bombardment isn't the only death's cause in Gaza.  According 2 Euro-Mediterranean Human Rights Monitor, 71% Gazans r facing food shortage, 98% suffer from starvation while 64% consume grass, raw & or expired food. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
+        //   type: "image",
+        //   src: p8,
+        // },
         // {
         //   text: "فرحة الأطفال أعيادهم. إلى جانب جبرُ خاطرِ خليل وإسعاده، يظهر في هذا الفيديو إيثار الأسرة لخليل رغم كثرة عددهم وحاجتهم؛ لكنهم يثقون في عطاء الله الذي وسعت خزائنه السماوات والأرض. 'وَاللهُ فِي عَوْنِ الْعَبْدِ مَا كَانَ الْعَبْدُ فِي عَوْنِ أَخِيهِ'. رواه مسلم #ولعت #الهلال #ايران",
         //   type: "video",
