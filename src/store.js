f634d82db2d6a627d48a435bad9e15e2dca52138
afnan730 <afnan6642@gmail.com>
2023-12-31 20:27:22 +0300

    new year tweets

--- src/store.js
+++ src/store.js
@@ -1,80 +1,86 @@
 import { createStore } from "vuex";
 
 import v1 from "./assets/static/v1.mp4";
-// import v2 from "./assets/static/v2.mp4";
-import p4 from "./assets/static/p4.jpeg";
+import v2 from "./assets/static/v2.mp4";
+
 import p1 from "./assets/static/p1.jpeg";
-import p3 from "./assets/static/p3.jpeg";
+
 import p2 from "./assets/static/p2.jpeg";
-import p5 from "./assets/static/p5.jpeg";
-import p6 from "./assets/static/p6.jpeg";
-import p7 from "./assets/static/p7.jpeg";
-import p8 from "./assets/static/p8.jpeg";
-import p9 from "./assets/static/p9.jpeg";
-// import v3 from "./assets/static/v3.mp4";
-// import v4 from "./assets/static/v4.mp4";
-// import v5 from "./assets/static/v5.mp4";
-import p10 from "./assets/static/p10.jpeg";
+
+import v3 from "./assets/static/v3.mp4";
+import v4 from "./assets/static/v4.mp4";
+import v5 from "./assets/static/v5.mp4";
+import v6 from "./assets/static/v6.mp4";
+import v7 from "./assets/static/v7.mp4";
+import v8 from "./assets/static/v8.mp4";
+import v9 from "./assets/static/v9.mp4";
+
 const store = createStore({
   state() {
     return {
       tweets: [
         {
-          text: "King–Crane Commission stated:'the zionists claim that jews've the right 2 a land by being descendants from natives 2000+ yrs ago shouldn't be taken seriously&Jews immigration should be limited&2 stop making Palestine a purely Jew's state' Aug 29,1919 #Term #Palmer #Chelsea #Luton",
+          text: "Happy New Year from Gaza, where at least 11,800 children have been killed in the ongoing Israel indiscriminate attacks! Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
           type: "image",
-          src: p5,
+          src: p2,
         },
         {
-          text: "Understanding Palestinians' cause isn't so complicated. They're the indigenous of a land & a group of Zionists who stole it to establish their state. But that wasn’t enough 4 those homes wreckers, as they're trying to wipe Palestinians as well! #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p3,
+          text: "يد واحدة لا تصفق؛ لكنها تخوض معركة، وتسحق جيشا، وتصنع بطولة عجزت عن صناعتها أمة. قدم واحدة لا تستطيع السير؛ لكنها تستطيع الوقوف في أرض الرباط تدافع عن ثغر أمة. الإقدام أو التراجع من داخل المرء، ولا علاقة له بفقدان أو تعطل عضو. #السنه_الجديده #HappyNewYear #الشهيد_الساجد آرسنال",
+          type: "video",
+          src: v4,
         },
         {
-          text: "Palastinians in Israeli's prisons suffer from  bad and inhumane conditions. Lately, they are being treated as lab rates & forced to participate in drugs' trail that worsened their health. #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p7,
+          text: "The creation of a “buffer state” that would be hostile to it's neighbours and friendly to Europe and it's interests.\n- Al-Nakba: The Palestinian catastrophe documentry. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v2,
         },
         {
-          text: "Killing, for them, is a religious ideology!What logic is there to justify genocides?Where are all the human rights organizations regarding what's happening in Gaza? Aren't they supposed to stand up with the oppressed & protect them? #Term #Palmer #Chelsea #Luton #Gaza",
+          text: "While we r busy preparing to celebrate the new year & share our dreams, hopes, parties with friends & relatives, we should take a moment 2 imagine how Gazan d receive it after 87 days of continuous bombardment. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
           type: "video",
           src: v1,
         },
         {
-          text: "Ben Gurion,the founder of Israel,said:'I support the forced deportation of Palestinians,& I don't see anything immoral in it.' So,4 Israelis, Palestinians've no right 2 Palastine & they r the obstacle 2 a purely Jewish State's establishment. #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p4,
+          text: "Isreali soldiers are mocking, scorning and bombing a Gazan's masjid with an open disregard of any religion's beliefs and holly places. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v7,
         },
+
         {
-          text: "Israel frequently engages in projection, falsely accusing the resistance of using children as human shields. In facts, evidence proves that the Israeli army employs this tactic, and use children to shield their tanks, among other instances. #Term #Palmer #Chelsea #Luton #Gaza.",
+          text: "More than 800 prisoners, in the Israeli occupation' prisons, suffer from illness and deliberate neglect in & 250 of them have chronic diseases. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
           type: "image",
           src: p1,
         },
         {
-          text: "Being a Gazan, you are deemed a terrorist & a target. Occupation's army kills indiscriminately everyone (children,  women, elderly, etc) & destroyed everything in their way and then create a new fabrication to mask the truth. #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p2,
+          text: "To revenge its army defeats by the Resistance, Israeli occupation tortures the defenseless civilians in open disregard to all morals, principles & international laws. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v3,
+        },
+        {
+          text: "Isrealis've been carrieng out war crimes against Palestinians 4 decades: humiliation, torture, forced replacement&killing. Such practices r what lead 2 Oct 7th which's a reaction of decades of oppression&injustices. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v8,
         },
 
         {
-          text: "Washington & Europe's support 4 Israeli occupation in its barbaric attacks on Gaza, where no one is safe,is a clear indication of double standards & hypocrisy. It is the real language that major players & politicians speak & understand #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p6,
+          text: "As the world celebrates the Happy New Year, thousands of children in Gaza are being killed. The genocide on Gaza's persisted for 87 days, and those who survive live in panic & displacement, enduring Israeli bombings in the open. Happy 2024 #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v9,
         },
 
+        // {
+        //   text: "According to the Palestinian Red Crescent spokesman, the entire northern Gaza Strip lacks a single ambulance, despite the northern Gaza Strip is crowded with wounded and martyrs daily as a result of the ongoing criminality of the occupation. #Term #Palmer #Chelsea #Luton #Gaza",
+        //   type: "image",
+        //   src: p9,
+        // },
         {
-          text: "According to the Palestinian Red Crescent spokesman, the entire northern Gaza Strip lacks a single ambulance, despite the northern Gaza Strip is crowded with wounded and martyrs daily as a result of the ongoing criminality of the occupation. #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p9,
+          text: "As the Gaza genocide persists,our  new year won't be happy while countless innocents're being massacred by Israeli occupation. Instead of a countdown for a happy new year, let's countdown for a permanent #CEASEFIRE. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v6,
         },
         {
-          text: "Bombardment isn't the only death's cause in Gaza.  According to Euro-Mediterranean Human Rights Monitor, 71% Gazans are facing food shortage, 98% suffer from starvation while 64% consume grass, raw & or expired food. #Term #Palmer #Chelsea #Luton #Gaza",
-          type: "image",
-          src: p8,
-        },
-        {
-          text: "Gaza isn't Israel's sole target. Masjid AlAqsa faces continuous aggression 4 decades; on 12/28/2023, 174 settlers stormed it without restraint. Muslims endure inhumane treatment, humiliation, aggressive searches, assault,& unjustified entry denials. #Term #Palmer #Chelsea #Luton",
-          type: "image",
-          src: p10,
+          text: "With burns, suffering and fear caused by the heinous acts of Israeli occupation, this is how the children of Gaza will be starting their New Year. Happy 2024 Happy New Year #AEWWorldsEnd #Lions #FULARS Feliz Año",
+          type: "video",
+          src: v5,
         },
         // {
         //   text: "Even the bodies of martyrs in Gaza did not escape the brutality of the occupying entity, as they were mutilated, their internal organs were stolen, and many of them decomposed. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
@@ -86,11 +92,6 @@ const store = createStore({
         //   text: "Bombardment isn't the only death's cause in Gaza.  According 2 Euro-Mediterranean Human Rights Monitor, 71% Gazans r facing food shortage, 98% suffer from starvation while 64% consume grass, raw & or expired food. #Slavery #Christmas #Pamplona #XRC20 #Nigeria",
         //   type: "image",
         //   src: p8,
-        // },
-        // {
-        //   text: "فرحة الأطفال أعيادهم. إلى جانب جبرُ خاطرِ خليل وإسعاده، يظهر في هذا الفيديو إيثار الأسرة لخليل رغم كثرة عددهم وحاجتهم؛ لكنهم يثقون في عطاء الله الذي وسعت خزائنه السماوات والأرض. 'وَاللهُ فِي عَوْنِ الْعَبْدِ مَا كَانَ الْعَبْدُ فِي عَوْنِ أَخِيهِ'. رواه مسلم #ولعت #الهلال #ايران",
-        //   type: "video",
-        //   src: p1,
         // },
       ],
       arabic: {
