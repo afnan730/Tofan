cf602b32d7995de5a846a211486516b0d9deea26
afnan730 <afnan6642@gmail.com>
2024-01-04 22:45:32 +0300

    error handeling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -208,8 +208,8 @@ export default {
         }
         
       }).catch(e => {
-        console.log(e.response.data.arabic)
-        if(e.response.data=="<html> <head><title>504 Gateway Time-out</title></head> <body> <center><h1>504 Gateway Time-out</h1></center> <hr><center>nginx</center> </body> </html> <!-- a padding to disable MSIE and Chrome friendly error page --> <!-- a padding to disable MSIE and Chrome friendly error page --> <!-- a padding to disable MSIE and Chrome friendly error page --> <!-- a padding to disable MSIE and Chrome friendly error page --> <!-- a padding to disable MSIE and Chrome friendly error page --> <!-- a padding to disable MSIE and Chrome friendly error page -->"){
+        console.log(e)
+        if(error.response && error.response.status === 504){
           this.message="تم النشر بحمد الله"
           return;
         }
