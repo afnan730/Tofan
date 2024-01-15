dfacbcb07143d41898df58bff0a6994e8703231e
afnan730 <afnan6642@gmail.com>
2024-01-15 17:41:56 +0300

    productionSourceMap

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  productionSourceMap: true,
+  productionSourceMap: false,
   configureWebpack: {
     resolve: {
       fallback: {
-        stream: require.resolve('stream-browserify'),
-        http: require.resolve('http-browserify'),
-        url: require.resolve('url'),
-        zlib: require.resolve('browserify-zlib'),
-        "crypto": require.resolve("crypto-browserify"),
-        "https": require.resolve("https-browserify"),
-        "fs": false
+        stream: require.resolve("stream-browserify"),
+        http: require.resolve("http-browserify"),
+        url: require.resolve("url"),
+        zlib: require.resolve("browserify-zlib"),
+        crypto: require.resolve("crypto-browserify"),
+        https: require.resolve("https-browserify"),
+        fs: false,
       },
     },
   },
