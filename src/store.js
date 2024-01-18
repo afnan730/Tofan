b1dc9f033bd5fdd5bd4ecc405d6df36a7ea73687
Kaser2023 <75515943+Kaser2023@users.noreply.github.com>
2024-01-18 20:47:11 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -15,7 +15,7 @@ import v3 from "./assets/static/v3.mp4";
 import v4 from "./assets/static/v4.mp4";
 import v5 from "./assets/static/v5.mp4";
 import v6 from "./assets/static/v6.mp4";
-import v7 from "./assets/static/v7.mp4";
+// import v7 from "./assets/static/v7.mp4";
 import v8 from "./assets/static/v8.mp4";
 import v9 from "./assets/static/v9.mp4";
 import v10 from "./assets/static/v10.mp4";
