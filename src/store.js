8e0f69c8a5995be413949b0dcd4fcdb019b9fdba
Afnan Abdo <79424831+afnan730@users.noreply.github.com>
2024-01-24 21:18:50 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore } from "vuex";
 
-
 // import p1 from "./assets/static/p1.jpg";
 import p2 from "./assets/static/p2.jpg";
 import p3 from "./assets/static/p3.jpg";
@@ -33,84 +32,68 @@ const store = createStore({
         //   src: p5,
         // },
 
-    //1
-    // {
-    //   text: "Israel consistently violates Human Rights.Ayman is a human rights workers & among the prisoners recounts abuse in Israeli detention where 6 Palestinian prisoners deaths reported  #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
-    //   type: "image",
-    //   link: "https://www.theguardian.com/world/2024/jan/21/gaza-activist-tells of-beating-and-abuse-in-israeli-detention CMP=Share_AndroidApp_Other&s=09",
-    //   src: p1,
-    // },
+        // {
+        //   text: "Israel consistently violates Human Rights.Ayman is a human rights workers & among the prisoners recounts abuse in Israeli detention where 6 Palestinian prisoners deaths reported  #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
+        //   type: "image",
+        //   link: "https://www.theguardian.com/world/2024/jan/21/gaza-activist-tells of-beating-and-abuse-in-israeli-detention CMP=Share_AndroidApp_Other&s=09",
+        //   src: p1,
+        // },
 
-    //2
-    {
+        //2
+        {
+          text: "This is how Israeli army has functioned in Palestine for so many years. #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
+          type: "video",
+          src: v1,
+        },
 
-      text: "This is how Israeli army has functioned in Palestine for so many years. #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
-      type: "video",
-      src: v1,
-    },
-	
-    //3
-    {
+        //3
+        {
+          text: "Israel practices racial segregation obviously. Not only in its refusal to recognize human rights, but also in its marginalization and use of Palestinans for purposes that contradict international treaties. #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
+          type: "video",
+          src: v2,
+        },
 
-      text: "Israel practices racial segregation obviously. Not only in its refusal to recognize human rights, but also in its marginalization and use of Palestinans for purposes that contradict international treaties. #Gazagenocide‌ $joker Emergencies Act #ALGMRT Mauritanie Trudeau Zverev",
-      type: "video",
-      src: v2,
-    },
-    
-    //4
-    {
+        //4
+        {
+          text: "Nazism is currently a historical Nazi, a revival of an ancient Nazi rife with genocide. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
+          type: "video",
+          src: v3,
+        },
 
+        //5
+        {
+          text: "This British lady answers questions related to double standards when it comes to non-white people & their interests, & why that appears in the dirtiest ways especially in issues related to Israel & Ukraine #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
+          type: "video",
+          src: v4,
+        },
 
-      text: "Nazism is currently a historical Nazi, a revival of an ancient Nazi rife with genocide. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
-      type: "video",
-      src: v3,
-    },
-    
-    //5
-    {
+        //6
+        {
+          text: "(I’m losing the desire to belong to a country that kills 11,000 children) a well-known Israeli playwright & screenwriter, Motti Lerner, told a crowd at a literary event in Israel. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
+          type: "video",
+          src: v5,
+        },
 
-      text: "This British lady answers questions related to double standards when it comes to non-white people & their interests, & why that appears in the dirtiest ways especially in issues related to Israel & Ukraine #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
-      type: "video",
-      src: v4,
-    },
+        //7
+        {
+          text: "Israel is using starvation of civilians as a method of warfare, and according to the U.N reports: 1 in 4 people are starving and 9 out of 10 families in some areas spend a day and night without food. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
+          type: "image",
+          src: p2,
+        },
 
-   
-    //6
-    {
+        //8
+        {
+          text: "Gaza Media Office:800,000 residents in Gaza & the northern enclave face death due to Israel's starvation & thirst policy. World leaders must act 2 end this abhorrent war crime with devastating effects on the innocents #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
+          type: "image",
+          src: p3,
+        },
 
-      text: "(I’m losing the desire to belong to a country that kills 11,000 children) a well-known Israeli playwright & screenwriter, Motti Lerner, told a crowd at a literary event in Israel. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev Mauritanie",
-      type: "video",
-      src: v5,
-    },
-    
-    
-      //7
-    {
-      text : "Israel is using starvation of civilians as a method of warfare, and according to the U.N reports: 1 in 4 people are starving and 9 out of 10 families in some areas spend a day and night without food. #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
-      type: "image",
-      src: p2,
-    },
-    
-       //8
-    {
-
-      text: "Gaza Media Office:800,000 residents in Gaza & the northern enclave face death due to Israel's starvation & thirst policy. World leaders must act 2 end this abhorrent war crime with devastating effects on the innocents #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
-      type: "image",
-      src: p3,
-    },
-    
-    
-      //9
-    {
-      
-      text: "Israel's deliberately blocking water,food,& fuel delivery while willfully impeding humanitarian assistance,apparently razing agricultural areas,& depriving the civilians of objects indispensable to their survival #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
-      type: "image",
-      src: p4,
-    },
-    
-    
-  
-    
+        //9
+        {
+          text: "Israel's deliberately blocking water,food,& fuel delivery while willfully impeding humanitarian assistance,apparently razing agricultural areas,& depriving the civilians of objects indispensable to their survival #Gazagenocide‌ $joker Emergencies Act Trudeau #ALGMRT Zverev",
+          type: "image",
+          src: p4,
+        },
       ],
       arabic: {
         header: "🔻طُـوفَـان الـحَـقِـيـقَـة",
