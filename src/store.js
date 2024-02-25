6f734ff5d115dd5d6a1978839e145eec9169b97e
Kaser2023 <75515943+Kaser2023@users.noreply.github.com>
2024-02-25 20:20:02 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import { createStore } from "vuex";
 
 import p1 from "./assets/static/p1.jpg";
 import p2 from "./assets/static/p2.jpg";
-import p3 from "./assets/static/p3.jpg";
-import p4 from "./assets/static/p4.jpg";
+// import p3 from "./assets/static/p3.jpg";
+// import p4 from "./assets/static/p4.jpg";
 // import p5 from "./assets/static/p5.jpg";
 // import p6 from "./assets/static/p6.jpeg";
 
@@ -11,10 +11,10 @@ import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
 import v3 from "./assets/static/v3.mp4";
 import v4 from "./assets/static/v4.mp4";
-// import v5 from "./assets/static/v5.mp4";
+import v5 from "./assets/static/v5.mp4";
 import v6 from "./assets/static/v6.mp4";
-// import v7 from "./assets/static/v7.mp4";
-// import v8 from "./assets/static/v8.mp4";
+import v7 from "./assets/static/v7.mp4";
+import v8 from "./assets/static/v8.mp4";
 // import v9 from "./assets/static/v9.mp4";
 // import v10 from "./assets/static/v10.mp4";
 // import v11 from "./assets/static/v11.mp4";
@@ -30,7 +30,8 @@ const store = createStore({
     // 1
 
     {
-      text: "'احنا جعانين.. ما ضل إلا القطط نأكلها'.. فلسطيني في شمال قطاع غزة يتحدث على المجاعة.",
+
+	text: "'All the Israeli villages in the country were built above Palestinian villages' A truth said by an Israeli citizen,so after all of this,is it rational for Palestinians to live in peace & safety with the occupation?",
       type: "video",
       src: v1,
     },
@@ -38,9 +39,8 @@ const store = createStore({
     //2
     {
 
-
-      text: "Israeli soldier posts video demolishing Gaza's only cancer hospital, then wrote ' it broke accidentally'. The hospital served as an Israeli camp for 3 months; no official claims of findings inside.",
-      type: "video",
+text: "An Israeli soldier documented Israeli occupation soldiers blowing up civilian homes in Khan Yunis while they mock a laugh, proud of their barbaric achievement!",
+type: "video",
       src: v2,
     },
 
@@ -49,8 +49,8 @@ const store = createStore({
     {
 
 
-      text: "من يخبر أهل الأردن أنّ نصف النصر رهين بيدهم الآن فقط ؟ من يخبرهم أن بيدهم إنقاذ 700 ألف نفس يرابطون في شمال غزة منذ 4 أشهر ونصف، والجوع ينهش لحومهم وعظامهم ؟ من يخبرهم أنهم لو أنقذوا هؤلاء، فلن يُكتب لإسرائيل النصر في هذه الحرب، ولو فعلت ما فعلت ؟!",
-      type: "video",
+	    text: "This is the treatment of the occupation towards Muslims when they want to perform worship at Al-Aqsa Mosque.",
+type: "video",
      src: v3,
     },
     
@@ -58,24 +58,26 @@ const store = createStore({
     // 4
     
     {
-      text: "The World Ignores Israel's War Crimes. It's time to question the narrative of 'self-defense.' Is this truly defense, or are we witnessing an ongoing cycle of oppression?",
-      type: "video",
+
+	    text: "“What’s happening in Gaza is not war. It’s genocide.” —Brazil’s President Lula da Silva",
+	    type: "video",
       src: v4,
       },
     
     //5
 
-    // {
-    //   text: "This is how the occupation deals with Muslims wanting to pray at Al-Aqsa Mosque, whether during Ramadan or throughout the year. Yet, some still hope to live in peace with a military raised on criminality.",
-    //   type: "video",
-    //   path: v5,
-    // },
+     {
+
+	     text: "@realRickWiles talks about what he had seen when he went to occupied Palestine in 2018! 'If we don't stop Zionism, someday we'll all be Palestinians, & if Zionism isn't stopped, the entire world will become Palestinians...'",
+	     type: "video",
+       src: v5,
+     },
 
     //   //6
     {
-      text: "هل تعلم ماذا يأكل الناس شمال غزة؟ - أعلاف الأرانب - التبن مخلوطًا بالشعير - خبزٌ عفن مغسولٌ بالماء - خضارٌ ملوّث بالفوسفور والذخائر- جذور النباتات - الخبيزة والسلق مخلوطة بالأعشاب.",
-      type: "text",
-     // path: ["v4.mp4"],
+text : "UN report just released shows Israel has kidnapped women, held women in cages, stripped them naked and released footage online, severely beat women, and raped women in the occupied West Bank.",
+	    type: "video",
+      src: v6,
     },
     
     
@@ -83,36 +85,41 @@ const store = createStore({
  
        {
 
-         text: "Do you know what people in northern Gaza eat? -Rabbit feed -Straw mixed with barley, -Moldy bread washed with water, -Vegetables contaminated with phosphorus & bullets, -Plant roots -Wild plants mixed with herbs. #NorthernGazaStarving",
-         type: "text",
+
+	       text: "Israel arrest a 5 year old Palestinian boy, over 700. Palestinian children are illegally detained every year!",
+	       type: "video",
+	       src: v7,
         },
 
+	       // 8
+
+    {
+
+	    text: "Josep Borrell,the EU's High Representative for Foreign Affairs,known for his inconsistent stance on Israeli crimes, exposes the West & countries supporting Israel regarding war crimes & Netanyahu's plan to invade Rafah",
+	    type: "video",
+      src: v8,
+    },
     
-    //	8
+    //	9
 
       {
-        text: "This represents true terrorism and the actions of deranged killers, not condemned by the world that claims to fight terrorism while oppressing peoples and occupying cities.",
-        type: "image",
+
+	text: "The one who called for the rape of non-Jewish women is the Israeli army rabbi, and the one who committed sexual assaults, according to the United Nations, is also Israel. Stop misleading people",
+	      type: "image",
            src: p1,
        },
     
 
-    //9
+    //10
 
     {
 
-      text: "Israeli soldiers not only engaged in sexual assault against women in Gaza but also shamelessly displayed their misconduct to the world.Meanwhile,Israeli propaganda wrongly accuses Hamas of assaulting women without evidence.",
-      type: "image",
+text: "A thief is not an owner.",
+	    type: "image",
       src: p2,
     },
 
-    // 10
-
-    {
-      text: "Hope for international justice endures globally despite decades of Palestinian suffering under a system rooted in colonization & oppression. Doubts arise about expecting justice from those perpetrating such actions.",
-      type: "video",
-      src: v6,
-    },
+   
 
    
     
