edb2d18e03c3af379d53c675947625ef82877889
Kaser2023 <75515943+Kaser2023@users.noreply.github.com>
2024-03-10 20:07:29 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from "vuex";
 
 import p1 from "./assets/static/p1.jpg";
-// import p2 from "./assets/static/p2.jpg";
+import p2 from "./assets/static/p2.jpg";
 // import p3 from "./assets/static/p3.jpg";
 // import p4 from "./assets/static/p4.jpg";
 // import p5 from "./assets/static/p5.jpg";
@@ -9,13 +9,13 @@ import p1 from "./assets/static/p1.jpg";
 
 import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
-import v3 from "./assets/static/v3.mp4";
-import v4 from "./assets/static/v4.mp4";
-import v5 from "./assets/static/v5.mp4";
+// import v3 from "./assets/static/v3.mp4";
+// import v4 from "./assets/static/v4.mp4";
+// import v5 from "./assets/static/v5.mp4";
 import v6 from "./assets/static/v6.mp4";
 import v7 from "./assets/static/v7.mp4";
 import v8 from "./assets/static/v8.mp4";
-// import v9 from "./assets/static/v9.mp4";
+import v9 from "./assets/static/v9.mp4";
 import v10 from "./assets/static/v10.mp4";
 // import v11 from "./assets/static/v11.mp4";
 // import v12 from "./assets/static/v12.mp4";
