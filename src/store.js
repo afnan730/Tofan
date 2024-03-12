6d4a54fdd6b8638c93cdc2cfea18ab8562f774c2
afnan730 <afnan6642@gmail.com>
2024-03-12 21:26:57 +0300

    new posts

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 
-import p1 from "./assets/static/p1.jpg";
+import p1 from "./assets/static/p1.jpeg";
 import p2 from "./assets/static/p2.jpg";
 // import p3 from "./assets/static/p3.jpg";
 // import p4 from "./assets/static/p4.jpg";
@@ -9,13 +9,13 @@ import p2 from "./assets/static/p2.jpg";
 
 import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
-// import v3 from "./assets/static/v3.mp4";
-// import v4 from "./assets/static/v4.mp4";
-// import v5 from "./assets/static/v5.mp4";
+import v3 from "./assets/static/v3.mp4";
+import v4 from "./assets/static/v4.mp4";
+import v5 from "./assets/static/v5.mp4";
 import v6 from "./assets/static/v6.mp4";
 import v7 from "./assets/static/v7.mp4";
-import v8 from "./assets/static/v8.mp4";
-import v9 from "./assets/static/v9.mp4";
+// import v8 from "./assets/static/v8.mp4";
+// import v9 from "./assets/static/v9.mp4";
 // import v10 from "./assets/static/v10.mp4";
 // import v11 from "./assets/static/v11.mp4";
 // import v12 from "./assets/static/v12.mp4";
@@ -24,97 +24,78 @@ import v9 from "./assets/static/v9.mp4";
 // import v11 from "./assets/static/v11.mp4";
 // import v12 from "./assets/static/v12.mp4";
 
-
 const store = createStore({
   state() {
     return {
       tweets: [
+        // 1
 
-
-    
-    // 1
-
-    {
-      text: "Graphic content: Israeli military-affiliated accounts celebrate feeding dogs with the remains of Palestinians in Gaza.",
-	type: "video",
-      src: v1,
-    },
-
-    //2
-    {
-     text: "Israel openly commits & admits its crimes,films soldiers boasting about many. Despite global institutions,laws remain elusive for everyone. Efforts to prove Gazans' suffering persist,yet trials favor the perpetrator over the victim.", 
-type: "video",
-      src: v2,
-    },
-
-    
-    //   //	3
-    {
-     text: "He was killed by the Israeli terrorist army while getting a bag of flour from the top of the truck in northern Gaza.", 
-	type: "image",
-     src: p1,
-    },
-    
-    
-    // 4
-    
-    {
-     text: "They dehumanize Palestinians as animals,but the truth is that the occupying soldiers R the ones behaving like animals,demolishing everything they encounter! This isn't self-defense;it's a complete ethnic cleansing", 
-	    type: "video",
-      src: v6,
-      },
-    
-    //5
-
-     {
-      text: "Israeli bulldozers are demolishing homes and infrastructure during the ongoing raid in Tulkarm refugee camp, West Bank.",
-	     type: "video",
-       src: v7,
-     },
-
-    //   //6
-    {
-
-      text:"Zionist forces arrest,humiliate,& kill the innocents then steal their organs. These citizens resist leaving their homes for 2 reasons: they have no place to go, & they oppose forced displacement in any form.",
-	    type: "image",
-      src: p2,
-    },
-    
-    
-    //  7
- 
-       {
-      text: "In the West Bank town of Qabatya, Jenin: Israeli occupation forces are kidnapping a 7-year-old child.",
-	       type: "video",
-	       src: v8,
+        {
+          text: "This is what those in power have decided will be the new normal!",
+          type: "image",
+          src: p1,
         },
 
-	       // 8
+        //2
+        {
+          text: "Speaking of double standards! When it's Ukraine, the world opens its eyes, but when it's Gaza, everyone seems to have turned blind.",
+          type: "video",
+          src: v1,
+        },
 
-    {
-      text: "Settlement leaders are among the most inciters for starving and displacing the people of Gaza.",
-	    type: "video",
-      src: v9,
-    },
-    
-    // //	9
+        //	3
+        {
+          text: "Helicopter footage reveals extensive destruction, highlighting that this is more of a genocide than the claimed 'self-defense.'",
+          type: "video",
+          src: v2,
+        },
 
-    //   {
-    //      text: "It is very important to follow the dialogue to refute the lies and claims of the occupation.",
-	   //    type: "video",
-    //        src: v10,
-    //    },
-    
+        // 4
 
-  
+        {
+          text: "The occupation has not stopped targeting homes and neighborhoods, so do not believe the lies about the cessation of the barbaric bombardment carried out by the occupation day and night on Gaza.",
+          type: "video",
+          src: v3,
+        },
 
+        //5
 
+        {
+          text: "The occupation continues its campaigns of arrests and raids without any justifiable reasons.",
+          type: "video",
+          src: v4,
+        },
 
-	      
+        //   //6
+        {
+          text: "This is the life of the people of Jerusalem 'alquds' throughout the occupation. Israeli occupation police officers in occupied Jerusalem restrain 6 Palestinians together and humiliate them as they are led away.",
+          type: "video",
+          src: v5,
+        },
 
-        
-       
-  
+        //  7
+
+        {
+          text: "'Yes, these R difficult scenes 4 viewers'. These martyrs were pulled out from the Nasser Medical Complex courtyard in Khan Younis after the graves were excavated & the bodies of the martyrs were desecrated by the occupying army",
+          type: "video",
+          src: v6,
+        },
+
+        // 8
+
+        {
+          text: "Palestine was not a land without people. There was a life on this land.",
+          type: "video",
+          src: v7,
+        },
+
+        // //	9
+
+        //   {
+        //      text: "It is very important to follow the dialogue to refute the lies and claims of the occupation.",
+        //    type: "video",
+        //        src: v10,
+        //    },
       ],
       arabic: {
         header: "🔻طُـوفَـان الـحَـقِـيـقَـة",
