dfa1ad7c4b57e514fa98e8f4df77f7b4ab530bf6
Afnan Abdo <79424831+afnan730@users.noreply.github.com>
2024-03-24 21:05:47 +0300

    Update store.js24/3

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore } from "vuex";
 
-// import p1 from "./assets/static/p1.jpeg";
-// import p2 from "./assets/static/p2.jpeg";
-// import p3 from "./assets/static/p3.jpeg";
-// import p4 from "./assets/static/p4.jpeg";
+import p1 from "./assets/static/p1.jpeg";
+import p2 from "./assets/static/p2.jpeg";
+import p3 from "./assets/static/p3.jpg";
+import p4 from "./assets/static/p4.jpg";
 // import p5 from "./assets/static/p5.jpg";
 // import p6 from "./assets/static/p6.jpeg";
 
@@ -13,9 +13,9 @@ import v3 from "./assets/static/v3.mp4";
 import v4 from "./assets/static/v4.mp4";
 import v5 from "./assets/static/v5.mp4";
 import v6 from "./assets/static/v6.mp4";
-import v7 from "./assets/static/v7.mp4";
+// import v7 from "./assets/static/v7.mp4";
 import v8 from "./assets/static/v8.mp4";
-import v9 from "./assets/static/v9.mp4";
+// import v9 from "./assets/static/v9.mp4";
 // import v10 from "./assets/static/v10.mp4";
 // import v11 from "./assets/static/v11.mp4";
 // import v12 from "./assets/static/v12.mp4";
@@ -30,58 +30,70 @@ const store = createStore({
       tweets: [
         // 1
         {
-          text: "A British surgeon who treated the wounded in Gaza tells how Israeli occupation forces burn children alive all the way to the bone.",
-          type: "video",
-          src: v1,
-        },
-        //2
-        {
-           text: "Their lies are evident yet again as they falsely claim there were military barracks inside Al-Shifa Hospital after their raid.",
-          type: "video",
-          src: v2,
-        },
-        //3
-        {
-          text: "'Constructing this port aims to displace Palestinians from Gaza, a grave act of ethnic cleansing.' British MP George Galloway condemns UK government's silence on occupation's intentions.",
+          text: "Horrifying testimonies have been coming out in the last 6 days of executions, rape, & torture at Al-Shifa Hospital and the vicinity of the medical complex",
           type: "video",
           src: v3,
         },
-        //4
+        //2
         {
-          text: "Jared Kushner's plan of excavating the Negev, establishing a safe zone, evacuating civilians from Gaza, then entering and completing the mission, is a clear and explicit strategy for ethnic cleansing and displacing people from their land.",
+          text: "[Our logic in this war is clear: don't leave any soul alive.] These are the instructions of their rabbis and distorted Jews.",
+          type: "image",
+          src: p1,
+        },
+        //3
+        {
+          text: "The occupation tries to hide the clear truth that 75 years of killing, displacement, settlement, and siege preceded the events of Oct 7th",
           type: "video",
           src: v4,
         },
+        //4
+        {
+          text: "Is digging up graves part of self-defense, or is it the reality of the Zionist culture based on genocide & the elimination of the indigenous people?!",
+          type: "video",
+          src: v2,
+        },
         //5
         {
-          text: "The international community is increasingly aware of Israel's use of famine as a weapon of war in Gaza, achieved through the blockade preventing aid from reaching the region.",
+          text: "142 sick, injured & others R trapped inside Al-Shifa Hospital, & the United Nations institutions didn't take any action, as if the people of Gaza R nothing!",
           type: "video",
-          src: v5,
+          src: v1,
         },
         //	6
         {
-        text: "October 7th was not the beginning of the Palestinian Genocide.",
-          type: "video",
-          src: v6,
+          text: "Little girl at Kamal Adwan Hospital in northern Gaza suffers from dehydration and malnutrition amidst the ongoing Israeli siege warfare.",
+          type: "image",
+          src: p2,
         },
         // 7
         {
-          text: "'Israel now ranks among the world's worst, committing unprecedented crimes.' - British Jewish journalist David Hearst",
-          type: "video",
-          src: v7,
+          text: "The prisoner Anas Mashal Al-Barghouti from the town of Kobar, north of Ramallah, has lost his memory due to severe torture inflicted by the Israeli occupation forces.",
+          type: "image",
+          src: p4,
         },
         //8
         {
-          text: "Apartheid Israeli occupation forces assault Palestinians trying to reach Al-Aqsa mosque in occupied Jerusalem.",
-          type: "video",
-          src: v8,
+          text: "The devastating Israeli bombardment of Gaza would not have been possible without tens of thousands of guided bombs and munitions sent by the United States since October 7",
+          type: "image",
+          src: p3,
         },
 
         //9
         {
-          text: "The head of an Israeli settlement in the northern West Bank calls for the erasure of Palestinian villages and cities in the West Bank, as seen in Gaza.",
+          text: "The occupation forces forced two children to undress as they passed from Gaza to its center, and on the way, saying that there were dozens of corpses killed by the Israeli occupation along their way",
           type: "video",
-          src: v9,
+          src: v5,
+        },
+        //10
+        {
+          text: "The annual crime has begun in Jerusalem, from beating and assaulting the people of Jerusalem and visitors of the Al-Aqsa Mosque.",
+          type: "video",
+          src: v6,
+        },
+        //11
+        {
+          text: "The Rabbi Eliyahu Mali and the Torah Genocide War: He advocates for the principle of killing everyone, including men, women, and children.",
+          type: "video",
+          src: v8,
         },
       ],
       arabic: {
