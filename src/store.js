de03d47ec1499f60cfdfdd3bb1fade8cc7626d5f
afnan730 <afnan6642@gmail.com>
2024-05-21 10:48:52 +0300

    21/5

--- src/store.js
+++ src/store.js
@@ -1,116 +1,84 @@
 import { createStore } from "vuex";
 
-import p1 from "./assets/static/p1.jpg";
-import p2 from "./assets/static/p2.jpg";
-// import p3 from "./assets/static/p3.jpeg";
-import p4 from "./assets/static/p4.jpeg";
-// import p5 from "./assets/static/p5.jpeg";
-import p6 from "./assets/static/p6.jpg";
-import p7 from "./assets/static/p7.jpg";
+import p1 from "./assets/static/p1.jpeg";
+// import p2 from "./assets/static/p2.jpg";
+// // import p3 from "./assets/static/p3.jpeg";
+// import p4 from "./assets/static/p4.jpeg";
+// // import p5 from "./assets/static/p5.jpeg";
+// import p6 from "./assets/static/p6.jpg";
+// import p7 from "./assets/static/p7.jpg";
 import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
 import v3 from "./assets/static/v3.mp4";
 import v4 from "./assets/static/v4.mp4";
-// import v5 from "./assets/static/v5.mp4";
-// import v6 from "./assets/static/v6.mp4";
-// import v7 from "./assets/static/v7.mp4";
-// import v8 from "./assets/static/v8.mp4";
+import v5 from "./assets/static/v5.mp4";
+import v6 from "./assets/static/v6.mp4";
+import v7 from "./assets/static/v7.mp4";
+import v8 from "./assets/static/v8.mp4";
 // import v9 from "./assets/static/v9.mp4";
-import v10 from "./assets/static/v10.mp4";
+// import v10 from "./assets/static/v10.mp4";
 // import v11 from "./assets/static/v11.mp4";
 // import v12 from "./assets/static/v12.mp4";
-import v17 from "./assets/static/v17.mp4";
-import v14 from "./assets/static/v14.mp4";
-import v11 from "./assets/static/v11.mp4";
-import v12 from "./assets/static/v12.mp4";
+// import v17 from "./assets/static/v17.mp4";
+// import v14 from "./assets/static/v14.mp4";
+// import v11 from "./assets/static/v11.mp4";
+// import v12 from "./assets/static/v12.mp4";
 
 const store = createStore({
   state() {
     return {
       tweets: [
         {
-          text: "A before-and-after picture of Ahmed from Al-Jalazoun refugee camp in Ramallah, taken following his release today after spending 20 months in Israeli occupation prisons",
-          type: "image",
-          src: p1,
-        },
-
-        //3
-        {
-          text: "Watch the evidence of torture on Palestinian prisoners from the Gaza Strip released from Israeli occupation prisons days ago",
-          type: "video",
-          src: v10,
-        },
-
-        //4
-        {
-          text: "It never started on the 7th of October.Israel’s land grab and ethnic cleansing have been ongoing for more than 75 years.",
-          type: "video",
-          src: v14,
-        },
-        //5
-
-        // 1
-        {
-          text: "The Criminal Israeli occupation army, supported by the US,UK and other European countries, is bombing the displaced Gazans in their tents.",
+          text: "American surgeon Dr. Adam El-Hamwy:'I've worked in war &disaster zones for 20 years,but I've never seen anything like Gaza. I performed more amputations on children in 2 weeks in Gaza than in my entire career!'",
           type: "video",
           src: v1,
         },
-        {
-          text: "Palestinian ex-prisoner Imad Jadallah from Dura, south of Hebron, upon release from Israeli jails today. The image highlights severe malnutrition, indicating Israeli authorities' denial of hygiene facilities as collective punishment after 7 Oct",
-          type: "image",
-          src: p7,
-        },
-        {
-          text: "The Commission of Detainees Affairs says it received yesterday a released prisoner who lost his mind as a result of torture in Israeli prisons.",
-          type: "image",
-          src: p6,
-        },
-        {
-          text: "I saw them tweezed the hair of his beard using pincers . A horrifying testimony by a recently released detainee about doctor Eyad Shaqoura.",
-          type: "video",
-          src: v12,
-        },
-        {
-          text: 'The witnesses of the newly released Palestinian prisoners stated: "We endured 24 hours of torture, oppression, and humiliation. We were surrounded by the Israeli army\'s fierce dogs, and Israeli jailers even urinated on us."',
-          type: "video",
-          src: v11,
-        },
-        {
-          text: "203 days of suffering in Gaza: families torn apart, homes destroyed, lives lost. It's time for the free people worldwide to esclate their actions, standing firm against this injustice",
-          type: "video",
-          src: v17,
-        },
-        //2
-        {
-          text: "Gaza, the Open Massacre",
-          type: "image",
-          src: p4,
-        },
 
         //3
         {
-          text: "The Israeli occupation army 's terrorism knows no bounds, targeting children and those aiding them, be they journalists or medical workers.",
+          text: "An expert presents investigations with evidence of the brutality of the occupation at Al-Shifa Hospital in Gaza.",
           type: "video",
           src: v2,
         },
 
         //4
         {
-          text: "How long will the suffering of these innocents continue, between displacement, killing, and hunger, while powerful countries continue to support the occupation in its acts of genocide and injustice?",
+          text: "Everything you need to know about Rafah 'the-safe-zone'.",
           type: "video",
           src: v3,
         },
         //5
+
+        // 1
         {
-          text: "Have we lost our minds to condemn the resistance of a people subjected to repression, murder, and genocide for 75 years?",
+          text: "ISRAEL TARGETS CHILDREN!",
           type: "video",
           src: v4,
         },
-        //	6
         {
-          text: "Israeli soldiers are torching homes in Jabalia. Posted minutes ago from a soldier on social media writing: 'Gaza is on fire'!",
+          text: "American influencer Guy Christensen:'Biden sent another billion dollars worth of weapons to Israel less than a week after saying he wouldn't send more weapons if Israel invaded Rafah. They keep lying & deceiving us!'",
+          type: "video",
+          src: v5,
+        },
+        {
+          text: "This 92-year-old Nakba survivor says the genocide in Gaza today is “history repeating itself.”",
+          type: "video",
+          src: v6,
+        },
+        {
+          text: "THE NAKBA EXPLAINED",
+          type: "video",
+          src: v7,
+        },
+        {
+          text: "Israel has become the most dangerous place for Jews to live.",
+          type: "video",
+          src: v8,
+        },
+        {
+          text: "The Israeli military's action of publicly releasing a list of citizens, including minors, alleging them to be Hamas informants is appalling. This includes kids whose personal information has been disclosed.",
           type: "image",
-          src: p2,
+          src: p1,
         },
       ],
       arabic: {
