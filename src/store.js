c88f38c649b6d5cd9ead2b92d2c986cde3b0c6e6
Kaser2023 <75515943+Kaser2023@users.noreply.github.com>
2024-05-29 20:31:43 +0300

    Posting 29.05

--- src/store.js
+++ src/store.js
@@ -1,18 +1,18 @@
 import { createStore } from "vuex";
 
-import p1 from "./assets/static/p1.jpeg";
-import p2 from "./assets/static/p2.jpeg";
-import p3 from "./assets/static/p3.jpeg";
-import p4 from "./assets/static/p4.jpeg";
-import p5 from "./assets/static/p5.jpeg";
+import p1 from "./assets/static/p1.jpg";
+import p2 from "./assets/static/p2.jpg";
+import p3 from "./assets/static/p3.jpg";
+import p4 from "./assets/static/p4.jpg";
+import p5 from "./assets/static/p5.jpg";
 // import p6 from "./assets/static/p6.jpg";
 // import p7 from "./assets/static/p7.jpg";
 import v1 from "./assets/static/v1.mp4";
 import v2 from "./assets/static/v2.mp4";
 import v3 from "./assets/static/v3.mp4";
 import v4 from "./assets/static/v4.mp4";
-// import v5 from "./assets/static/v5.mp4";
-// import v6 from "./assets/static/v6.mp4";
+import v5 from "./assets/static/v5.mp4";
+import v6 from "./assets/static/v6.mp4";
 // import v7 from "./assets/static/v7.mp4";
 // import v8 from "./assets/static/v8.mp4";
 // import v9 from "./assets/static/v9.mp4";
@@ -30,76 +30,90 @@ const store = createStore({
       tweets: [
         //1
         {
-          text: '"وبعد أيها المسلمون؟هؤلاء إخوانكم، وتلك نساؤكم وأعراضكم، من لم يحركه حرق المسلمين، وتقطيع أطفالهم أشلاءً، وتمزيق أوصالهم أحياءً.. فلا خير فيه! ووالله، وبالله، وتالله، من لم يوقظه هذا، لن يستيقظ بعد ذلك إلا في قبره!"',
-          type: "video",
-          src: v3,
-        },
-        //2
-        {
-          text: "Israeli & USA war against Children in Rafah. ",
+       text: "Israel's celebration of murdering innocent men, women, and children is beyond demonic. It's reminiscent of the actions of a serial killer, yet it's an entire nation condoning such atrocities.",
           type: "video",
           src: v1,
         },
-
-        //3
+        //2
         {
-          text: "Israel's attempt to justify the burning +100 innocent Palestinians alive by claiming to target a resistance member is a blatant violation of international law & basic human decency. #Rafah_Massacre ",
+      text: "This horror, this pure evil—this is Zionism. It's their agenda for every nation, every race, every man, woman, and child outside their cult. It's time to recognize: supporting Israel = supporting genocide.",
           type: "video",
           src: v2,
         },
 
+        //3
+        {
+      text: "The world must realize: Israel's terrorism won't stop until it's stopped. Bombing country after country, beheading child after child, they'll persist until the world stops them. #StopIsraelTerrorism" ,
+          type: "video",
+          src: v3,
+        },
+
         //4
         {
-          text: "Israel burned +100 Palestinians alive,mostly women & kids ,sparking outrage. Claiming targeting a resistance member,it evaded accountability. Media echoed the narrative,obscuring the truth. People's anger waned,forgetting the true massacre of 100! ",
+      text:"Palestinian child in Rafah targeted by Israeli drones. Zionists won't stop this repetitive bloody expansionism & targeting children until the world makes significant actions to stop them",
           type: "image",
           src: p1,
         },
 
         // 5
         {
-          text: "Despite overwhelming evidence of Israel's atrocities,its narrative of 'precision strikes' continues to be parroted by international media, obscuring the truth of the Rafah massacre ",
+      text: "Haaretz reports that Israel has kidnapped nearly 4000 citizens from Gaza since the start of the offensive on Gaza, only 1500 of whom have been freed while 2000 have been arrested." + random ,
           type: "image",
           src: p2,
         },
 
         // 6
         {
-          text: "Before u sleep,remember millions in Gaza don't wish food,cold drinks,or warm beds. All they wish is not to have their names among the charred bodies in tomorrow's or the day after's massacre committed by Israel",
-          type: "image",
-          src: p3,
+      text: "https://twitter.com/thetufan07/status/1795191366365905257?t=r7nEIEckcA6kGdpaILulXg&s=19 " ,
+          type: "text",
         },
 
         // 7
         {
-          text: "US Secretary of Defense: We sent precision munitions to Israel in order to reduce the number of civilian deaths in Gaza and hit targets accurately.",
+      text: "Footage dating back to October 9, 2023, depicts the barbaric Israeli bombardment of civilian homes in northern Gaza, where some buildings still harbor thousands of bodies trapped under the rubble to this day",
           type: "video",
-          src: v2,
+          src: v4,
         },
+
+        //8
         {
-          text: "Yes, indeed, your missiles have accurately hit their targets.",
+       text: "Israeli occupation snipers shot dead Palestinian civilians in Jabalia refugee camp, northern Gaza.",
+          type: "video",
+          src: v5,
+        },
+
+        //9
+        {
+      text: "Every child, every nation, every race is at risk if we don't resist. This is the start of a global catastrophe",
+          type: "video",
+          src: v6,
+        },
+
+        //10
+        {
+       text: "Important and necessary to know: - Rafah doesn't have hospitals, emergency services, & even facilities for injuries. - Rafah doesn't have morgue refrigeration or intensive care units. - In Rafah, anyone who gets infected now doesn't survive." ,
           type: "image",
+          src:p3,
+        },
+
+        //11
+        {
+      text: "https://twitter.com/fedaaadeen1/status/1795485186299244636?t=RnKA6EwxBvzWA9XLgXzc3g&s=19  ",
+          type: "text",
+        },
+
+        //12
+        {
+       text: "United States support Israel to behead children in Gaza",
+          type: "text",
           src: p4,
         },
+
+        //13
         {
-          text: "A child martyr whose features were altered by the occupation's missiles on the displaced people's tents in Rafah yesterday.",
-          type: "image",
+       text: "Mass exodus of a large number of citizens from areas that the occupation claimed were safe in Rafah, and despite that, it launched dozens of raids and committed horrific massacres, defying the international justice’s decision to stop its military operation in Rafah.",
+          type: "text",
           src: p5,
-        },
-        {
-          link: "https://x.com/thetufan07/status/1795111333249138871 ",
-          type: "text",
-        },
-        {
-          link: "https://x.com/thetufan07/status/1794826042835976368 ",
-          type: "text",
-        },
-        {
-          link: "https://x.com/DAlshmwsy/status/1794906615734218891 ",
-          type: "text",
-        },
-        {
-          link: "https://x.com/thetufan07/status/1794357369360842993 ",
-          type: "text",
         },
       ],
       arabic: {
